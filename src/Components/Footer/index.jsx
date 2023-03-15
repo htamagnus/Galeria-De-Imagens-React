60fd6bc9290319c789420075e50a28707b33e279
hta <agathamagnusg@hotmail.com>
2023-03-15 10:15:54 -0300

    add rel noreferrer

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -4,18 +4,20 @@ export default function Footer() {
   return (
     <footer className={styles.rodape}>
       <div className={styles.rodape__icones}>
-        <a href="https://github.com/htamagnus" target="_blank">
+        <a href="https://github.com/htamagnus" target="_blank" rel="noreferrer">
           <img
             src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
             width="40"
             height="40"
+            alt="Ícone do GitHub, na cor preta"
           />
         </a>
-        <a href="https://www.linkedin.com/in/agathamagnus/" target="_blank">
+        <a href="https://www.linkedin.com/in/agathamagnus/" target="_blank" rel="noreferrer">
           <img
             src="https://cdn-icons-png.flaticon.com/512/49/49408.png"
             width="40"
             height="40"
+            alt="Ícone do Linkedin, na cor preta"
           />
         </a>
       </div>
